c06d0f5a1f2f7f2f862ff6fc8c61d9205eeaadf9
SSDCc <35004442+SSDCc@users.noreply.github.com>
2018-07-23 14:01:40 +0800

    Add files via upload

--- link.py
+++ link.py
@@ -2,36 +2,29 @@
 import requests
 import time
 from tkinter import *
-import threading
 
 root=Tk()
 url = r'https://api.live.bilibili.com/ajax/msg'
 form ={'roomid': 5096,'csrf_token' : '8555f03a84aaf5c5599091cdc7959e0c'}
 alldm =[]
 
-def dmall(howdm = 0,alldm =[]):
+def dmall():
+    global alldm
+    text = ''
     html = requests.post(url,data=form)
     for dm in html.json()['data']['room']:
         if dm not in alldm:
             alldm.append(dm)
-        if len(alldm)  >howdm:
+        if len(alldm)  >10:
             alldm.pop(0)
-    return alldm
-
-def textchange():
-    global alldm
-    text = ''
-    time.sleep(1)
-    alldm = dmall(10,alldm)
-    print('--------------------------------------------------------------')
-    for dm in alldm:
-        print(dm['nickname'],' 说 ',dm['text'])
-        text = text+dm['nickname']+' 说 '+dm['text']+'\n'
-    text0.delete(1.0,END)
-    text0.insert(END, text)
-    root.after(500, textchange)
+        text = ''
+        for dmin in alldm:
+            text = text+dmin['nickname']+' 说 '+dmin['text']+'\n'
+        text0.delete(1.0,END)
+        text0.insert(END, text)
+    root.after(100, dmall)
 
 text0 = Text (width = 50,height=12)
 text0.pack()
-textchange()
+dmall()
 root.mainloop()
